310f9eca69d190d3e7c0f0b72cb70865ea49f286
Miguel Urena <urena_miguel82@outlook.com>
2022-11-28 14:50:02 -0800

    Add words update App.tsx delete unwanted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import { useState } from "react"
+import words from "./wordList.json"
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test")
-  return <h1>Hi</h1>
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  return (
+    <div 
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center"
+    }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+    </div>
+  )
 }
 
 export default App
