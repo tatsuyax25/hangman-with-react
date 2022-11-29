6e4ec48e32e16a055198380a5e132167e5c77a8f
Miguel Urena <urena_miguel82@outlook.com>
2022-11-29 15:09:57 -0800

    Design the post for the hangman

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,5 +1,8 @@
 export function HangmanDrawing() {
     return <div style={{ position: "relative" }}>
+        <div style={{ height: "50px", width: "10px", background: "black", position: "absolute", top: 0, right: 0 }} />
+        <div style={{ height: "10px", width: "200px", background: "black", marginLeft: "120px" }} />
+        <div style={{ height: "400px", width: "10px", background: "black", marginLeft: "120px" }}/>
         <div style={{ height: "10px", width: "250px", background: "black" }}/>
     </div>
 }
