aeaf53d4dbb0d9f5bcc7604b144115452277ec60
Miguel Urena <urena_miguel82@outlook.com>
2022-11-30 14:36:30 -0800

    Coding HangmanWord.tsx pt 2

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,9 +1,10 @@
 export function HangmanWord() {
     const word = "test"
+    const guessedLetters = ["t", "e", "g"]
     return <div style={{ display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace" }}>
         {word.split("").map((letter, index) => (
-            <span style={{ borderBottom: ".1em solid black" }}>
-                <span style={{ visibility: "hidden" }}>{letter}</span>
+            <span style={{ borderBottom: ".1em solid black" }} key={index}>
+                <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden", }}>{letter}</span>
             </span>
         ))}
     </div>
