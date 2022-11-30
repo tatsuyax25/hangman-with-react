1893e003933a407fdc451d639f80d205a0676bb8
Miguel Urena <urena_miguel82@outlook.com>
2022-11-30 14:19:31 -0800

    Coding HangmanWord.tsx

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,10 @@
 export function HangmanWord() {
-
+    const word = "test"
+    return <div style={{ display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace" }}>
+        {word.split("").map((letter, index) => (
+            <span style={{ borderBottom: ".1em solid black" }}>
+                <span style={{ visibility: "hidden" }}>{letter}</span>
+            </span>
+        ))}
+    </div>
 }
