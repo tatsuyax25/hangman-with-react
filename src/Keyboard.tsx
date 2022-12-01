747bd5120ca08c8590a08377ce258bdfdb16a7e1
Miguel Urena <urena_miguel82@outlook.com>
2022-12-01 15:08:26 -0800

    Coding Keyboard.tsx

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -29,6 +29,10 @@ const KEYS = [
 
 export function Keyboard() {
     return <div style={{ display: "grid", gridTemplateColumns: "repeat (auto-fit, minmax(75px, 1fr)", gap: ".5rem"}}>
-        
+        {KEYS.map(key => {
+            return (
+                <button key={key}>{key}</button>
+            )
+        })}
     </div>
 }
