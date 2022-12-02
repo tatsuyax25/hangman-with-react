80004107a954111c1f13cc9d334a878d83929f14
Miguel Urena <urena_miguel82@outlook.com>
2022-12-02 14:04:59 -0800

    Style buttons

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -33,7 +33,7 @@ export function Keyboard() {
     return <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", gap: ".5rem"}}>
         {KEYS.map(key => {
             return (
-                <button className={styles.btn} key={key}>{key}</button>
+                <button className={`${styles.btn}`} key={key}>{key}</button>
             )
         })}
     </div>
