cd51d938fee9dbff3dc21bcff78914bc244faaf7
Miguel Urena <urena_miguel82@outlook.com>
2022-12-06 15:02:43 -0800

    Update Keyboard logic

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ function App() {
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess}/>
       <div style={{ alignSelf: "stretch" }}>
-        <Keyboard />
+        <Keyboard activeLetter={guessedLetters.filter(letter => wordToGuess.includes(letter))} inactiveLetters={incorrectLetters} addGuessedLetter={addGuessedLetter} />
       </div>
     </div>
   )
