34629fa028940e8607d2bcb46ec0248311256b69
Miguel Urena <urena_miguel82@outlook.com>
2022-12-07 13:46:03 -0800

    Win/Lose Logic pt 3

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,10 @@ function App() {
   const isWinner = wordToGuess.split("").every(letter => guessedLetters.includes(letter))
 
   const addGuessedLetter = useCallback((letter: string) => {
-    if (guessedLetters.includes(letter)) return
+    if (guessedLetters.includes(letter) || isLoser || isWinner) return
 
     setGuessedLetters(currentLetters => [...currentLetters, letter])
-  }, [guessedLetters])
+  }, [guessedLetters, isWinner, isLoser])
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
